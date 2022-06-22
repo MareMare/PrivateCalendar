1ef1c80f11c5c5b01d0734c6d06210739c4723ed
MareMare <mare.fujii@gmail.com>
2022-06-23 00:09:36 +0900

    Update ExporterOptions.cs

--- src/CompanyCalendar.Exporter.Ics/ExporterOptions.cs
+++ src/CompanyCalendar.Exporter.Ics/ExporterOptions.cs
@@ -4,6 +4,8 @@ namespace CompanyCalendar.Exporter.Ics
 {
     public class ExporterOptions
     {
+        public static readonly string Key = "IcsExporter";
+
         public string FilePath { get; set; } = null!;
 
         public Encoding FileEncoding { get; set; } = Encoding.UTF8;
