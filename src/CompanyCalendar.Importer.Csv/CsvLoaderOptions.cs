5f56ed83b7e2c410905bcf0eca68f7677611d0d3
MareMare <mare.fujii@gmail.com>
2022-06-23 00:09:45 +0900

    Update CsvLoaderOptions.cs

--- src/CompanyCalendar.Importer.Csv/CsvLoaderOptions.cs
+++ src/CompanyCalendar.Importer.Csv/CsvLoaderOptions.cs
@@ -4,6 +4,8 @@ namespace CompanyCalendar.Importer.Csv
 {
     public class CsvLoaderOptions
     {
+        public static readonly string Key = "CsvLoader";
+
         public string FilePath { get; set; } = null!;
 
         public Encoding FileEncoding { get; set; } = Encoding.UTF8;
