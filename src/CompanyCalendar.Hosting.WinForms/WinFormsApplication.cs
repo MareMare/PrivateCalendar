bd5172bcdac151d014fca45577c40920f8faf4e8
MareMare <mare.fujii@gmail.com>
2022-06-23 00:10:47 +0900

    Add Project `CompanyCalendar.Hosting.WinForms`

---
+++ src/CompanyCalendar.Hosting.WinForms/WinFormsApplication.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyCalendar.Hosting.WinForms
+{
+    public static class WinFormsApplication
+    {
+        public static void Run<TMainForm>(IHostBuilder hostBuilder)
+            where TMainForm : Form
+        {
+            using var host = hostBuilder.Build();
+            using var serviceScope = host.Services.CreateAsyncScope();
+            var services = serviceScope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger(nameof(WinFormsApplication));
+            //var serviceProviderIsService = services.GetService<IServiceProviderIsService>();
+            try
+            {
+                logger.LogInformation("起動します。{a}", 10);
+
+                Application.Run(services.GetRequiredService<TMainForm>());
+
+                logger.LogInformation("終了しました。{a}", 10);
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "起動中に例外が発生しました。{ex}", ex.Message);
+            }
+        }
+    }
+}
