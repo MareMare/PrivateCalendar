9915e9ae6a6d0ac80b8e957725749a8185e9286c
MareMare <mare.fujii@gmail.com>
2022-06-28 15:34:22 +0900

    Add xml document comments.

--- src/CompanyCalendar.Exporter.Ics/IcsExporter.cs
+++ src/CompanyCalendar.Exporter.Ics/IcsExporter.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.Extensions.Options;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IcsExporter.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Options;
 
 namespace CompanyCalendar.Exporter.Ics
 {
+    /// <summary>
+    /// iCalendar ファイルへのエクスポートを提供します。
+    /// </summary>
     public class IcsExporter : IIcsExporter
     {
+        /// <summary>オプション構成を表します。</summary>
         private readonly IcsExporterOptions _options;
 
+        /// <summary>
+        /// <see cref="IcsExporter" /> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="options">オプション構成。</param>
         public IcsExporter(IOptions<IcsExporterOptions> options)
         {
             ArgumentNullException.ThrowIfNull(options);
@@ -14,10 +29,15 @@ namespace CompanyCalendar.Exporter.Ics
             this._options = options.Value;
         }
 
-        public async Task ExportAsync(string icsFilePath, IEnumerable<(DateTime date, string summary)> eventPairs, CancellationToken taskCancellationToken = default)
+        /// <inheritdoc />
+        public async Task ExportAsync(
+            string icsFilePath,
+            IEnumerable<(DateTime date, string summary)> eventPairs,
+            CancellationToken taskCancellationToken = default)
         {
             // [asp\.net \- How to create \.ics file using c\#? \- Stack Overflow](https://stackoverflow.com/questions/46033843/how-to-create-ics-file-using-c/46042482#46042482)
-            var pairs = eventPairs.Select(pair => (loweDate: pair.date, upperDate: pair.date.ToNextDay(), pair.summary)).ToArray();
+            var pairs = eventPairs.Select(pair => (loweDate: pair.date, upperDate: pair.date.ToNextDay(), pair.summary))
+                .ToArray();
             var generator = new IcsRuntimeTemplate(pairs)
             {
                 CalendarName = this._options.CalendarName,
@@ -27,10 +47,11 @@ namespace CompanyCalendar.Exporter.Ics
 
             var path = icsFilePath;
             var encoding = this._options.FileEncoding;
-            //await File.WriteAllTextAsync(path, ics, encoding, taskCancellationToken).ConfigureAwait(false);
+
+            // await File.WriteAllTextAsync(path, ics, encoding, taskCancellationToken).ConfigureAwait(false);
             using var stream = File.Open(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
             using var writer = new StreamWriter(stream, encoding);
             await writer.WriteAsync(ics.AsMemory(), taskCancellationToken).ConfigureAwait(false);
         }
-   }
+    }
 }
--- src/CompanyCalendar.Exporter.Ics/IcsRuntimeTemplate.partial.cs
+++ src/CompanyCalendar.Exporter.Ics/IcsRuntimeTemplate.partial.cs
@@ -1,17 +1,62 @@
-﻿namespace CompanyCalendar.Exporter.Ics
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IcsRuntimeTemplate.partial.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CompanyCalendar.Exporter.Ics
 {
+    /// <summary>
+    /// ICS の実行時テンプレートを表します。
+    /// </summary>
     public partial class IcsRuntimeTemplate
     {
-        private IList<(DateTime lowerDate, DateTime upperDate, string summary)> _eventPairs;
+        /// <summary>
+        /// <see cref="IcsRuntimeTemplate" /> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="pairs">開始日時、終了日時、イベント概要のタプルコレクション。</param>
         public IcsRuntimeTemplate(IEnumerable<(DateTime lowerDate, DateTime upperDate, string summary)>? pairs = null)
         {
-            this._eventPairs = (pairs ?? Array.Empty<(DateTime lowerDate, DateTime upperDate, string summary)>()).ToList();
+            this.EventPairs = (pairs ?? Array.Empty<(DateTime lowerDate, DateTime upperDate, string summary)>())
+                .ToList();
             this.Timestamp = $"{DateTime.Now.ToUniversalTime():yyyyMMddTHHmmssZ}";
         }
 
+        /// <summary>
+        /// プロダクト ID を取得または設定します。
+        /// </summary>
+        /// <value>
+        /// 値を表す <see cref="string" /> 型。
+        /// <para>プロダクト ID。既定値は <see langword="null" /> です。</para>
+        /// </value>
         public string ProductId { get; set; } = null!;
+
+        /// <summary>
+        /// カレンダー名を取得または設定します。
+        /// </summary>
+        /// <value>
+        /// 値を表す <see cref="string" /> 型。
+        /// <para>カレンダー名。既定値は <see langword="null" /> です。</para>
+        /// </value>
         public string CalendarName { get; set; } = null!;
+
+        /// <summary>
+        /// タイムスタンプの文字列を取得または設定します。
+        /// </summary>
+        /// <value>
+        /// 値を表す <see cref="string" /> 型。
+        /// <para>タイムスタンプの文字列。既定値は <see langword="null" /> です。</para>
+        /// </value>
         public string Timestamp { get; }
-        public IList<(DateTime lowerDate, DateTime upperDate, string summary)> EventPairs => this._eventPairs;
+
+        /// <summary>
+        /// イベントペアのコレクションを取得または設定します。
+        /// </summary>
+        /// <value>
+        /// 値を表す 開始日付、終了日付、サマリのタプル 型。
+        /// <para>イベントペアのコレクション。既定値は <see langword="null" /> です。</para>
+        /// </value>
+        public IList<(DateTime lowerDate, DateTime upperDate, string summary)> EventPairs { get; }
     }
 }
