60c808d418fb67279a26b25a306ef791581c86c8
MareMare <mare.fujii@gmail.com>
2022-07-20 16:19:28 +0900

    change to file-scoped namespace.

--- src/CompanyCalendar.App.WinForms/Progress/ServiceExtensions.cs
+++ src/CompanyCalendar.App.WinForms/Progress/ServiceExtensions.cs
@@ -7,36 +7,35 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CompanyCalendar.App.WinForms.Progress
+namespace CompanyCalendar.App.WinForms.Progress;
+
+/// <summary>
+/// <see cref="IServiceCollection" /> の拡張機能を提供します。
+/// </summary>
+public static class ServiceExtensions
 {
     /// <summary>
-    /// <see cref="IServiceCollection" /> の拡張機能を提供します。
+    /// <see cref="ProgressForm" /> に関するサービスの依存関係を追加します。
     /// </summary>
-    public static class ServiceExtensions
-    {
-        /// <summary>
-        /// <see cref="ProgressForm" /> に関するサービスの依存関係を追加します。
-        /// </summary>
-        /// <param name="services"><see cref="IServiceCollection" />。</param>
-        /// <returns>依存関係が追加された <see cref="IServiceCollection" />。</returns>
-        public static IServiceCollection AddProgressForm(this IServiceCollection services) =>
-            services.AddScoped<ProgressForm>();
+    /// <param name="services"><see cref="IServiceCollection" />。</param>
+    /// <returns>依存関係が追加された <see cref="IServiceCollection" />。</returns>
+    public static IServiceCollection AddProgressForm(this IServiceCollection services) =>
+        services.AddScoped<ProgressForm>();
 
-        /// <summary>
-        /// <see cref="ProgressForm" /> を使用します。
-        /// </summary>
-        /// <param name="serviceProvider"><see cref="IServiceProvider" />。</param>
-        /// <param name="owner">フォームを所有するトップレベルウィンドウ。</param>
-        /// <param name="configure"><see cref="ProgressForm" /> を構築するメソッドのデリゲート。</param>
-        /// <returns><see cref="IProgressScope" />。</returns>
-        public static IProgressScope UseProgressForm(
-            this IServiceProvider serviceProvider,
-            IWin32Window owner,
-            Action<ProgressForm>? configure = null)
-        {
-            var progressForm = serviceProvider.GetRequiredService<ProgressForm>();
-            configure?.Invoke(progressForm);
-            return progressForm.UseProgressFormScope(owner);
-        }
+    /// <summary>
+    /// <see cref="ProgressForm" /> を使用します。
+    /// </summary>
+    /// <param name="serviceProvider"><see cref="IServiceProvider" />。</param>
+    /// <param name="owner">フォームを所有するトップレベルウィンドウ。</param>
+    /// <param name="configure"><see cref="ProgressForm" /> を構築するメソッドのデリゲート。</param>
+    /// <returns><see cref="IProgressScope" />。</returns>
+    public static IProgressScope UseProgressForm(
+        this IServiceProvider serviceProvider,
+        IWin32Window owner,
+        Action<ProgressForm>? configure = null)
+    {
+        var progressForm = serviceProvider.GetRequiredService<ProgressForm>();
+        configure?.Invoke(progressForm);
+        return progressForm.UseProgressFormScope(owner);
     }
 }
